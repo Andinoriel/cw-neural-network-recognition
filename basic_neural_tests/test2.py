8e756c02b911dd963b9fad1a32c6bcf3614f0fad
Nicholas Simon <36269798+Andinoriel@users.noreply.github.com>
2019-03-25 17:50:29 +0200

    TEST #2: Simple code refactoring, 0.15
    Former-commit-id: ec3c9a2e5c3effe630e9bf4ada752d68cfe259c5

--- basic_neural_tests/test2.py
+++ basic_neural_tests/test2.py
@@ -7,6 +7,20 @@ from skimage import io
 
 check = True
 
+def loadImg(path):
+        read_img = io.imread(path)
+        reco_img = dlib.image_window()
+        reco_img.clear_overlay()
+        reco_img.set_image(read_img)
+        return [read_img, reco_img] 
+
+def displayReco(predictor, read_img, reco_img, j):
+        shape = predictor(read_img, j)
+        reco_img.clear_overlay()
+        reco_img.add_overlay(j)
+        reco_img.add_overlay(shape)
+        return shape
+
 def main():
         # shape_predictor - ready neuralnet for face recognition
         # face_recognition - ready model data using 68 face keypoints
@@ -15,17 +29,9 @@ def main():
         detector = dlib.get_frontal_face_detector()
 
         # loading first image
-        image_1 = io.imread('image_tests\\image1.jpg')
-        image_rec_1 = dlib.image_window()
-        image_rec_1.clear_overlay()
-        image_rec_1.set_image(image_1)
-
+        image_1, image_rec_1 = loadImg('image_tests\\image1.jpg')
         # loading second image
-        image_2 = io.imread('image_tests\\image2.jpg')
-        image_rec_2 = dlib.image_window()
-        image_rec_2.clear_overlay()
-        image_rec_2.set_image(image_2)
-
+        image_2, image_rec_2 = loadImg('image_tests\\image2.jpg')
 
         #determinate first image data
         determinators1 = detector(image_1, 1)
@@ -35,25 +41,18 @@ def main():
         # finding keypoints via shape_predictor & face_recognition for the first image
         for i,j in enumerate(determinators1):
                 print("Detection {}: LeftSide: {} TopSide: {} RightSide: {} BottomSide: {}".format(i, j.left(), j.top(), j.right(), j.bottom()))
-                shape = shape_predictor(image_1, j)
-                image_rec_1.clear_overlay()
-                image_rec_1.add_overlay(j)
-                image_rec_1.add_overlay(shape)
-
+                shape = displayReco(shape_predictor, image_1, image_rec_1, j)
         image_descriptor_1 = face_recognition.compute_face_descriptor(image_1, shape)
         
         # finding keypoints via shape_predictor & face_recognition for the second image
         for i,j in enumerate(determinators2):
                 print("Detection {}: LeftSide: {} TopSide: {} RightSide: {} BottomSide: {}".format(i, j.left(), j.top(), j.right(), j.bottom()))
-                shape = shape_predictor(image_2, j)
-                image_rec_2.clear_overlay()
-                image_rec_2.add_overlay(j)
-                image_rec_2.add_overlay(shape)
-
+                shape = displayReco(shape_predictor, image_2, image_rec_2, j)
         image_descriptor_2 = face_recognition.compute_face_descriptor(image_2, shape)
 
         # finding euclidean distance to compare the images
         descriptor_check = distance.euclidean(image_descriptor_1, image_descriptor_2)
+        print(descriptor_check)
         if descriptor_check < 0.6:
                 print('same human')
         else:
@@ -71,9 +70,9 @@ def checkInterrupt():
         
 
 if __name__ == '__main__':
-        main = threading.Thread(target=main)
+        current = threading.Thread(target=main)
         chckIntrpt = threading.Thread(target=checkInterrupt)
-        main.start()
+        current.start()
         chckIntrpt.start()
 else:
         pass
