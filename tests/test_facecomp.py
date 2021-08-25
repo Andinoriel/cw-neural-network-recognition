15f144433e09a23afd4797dfc387a52a9de34205
Mykola Symon <simonnikolaj20@gmail.com>
2021-08-25 18:12:07 +0300

    add face comparison tests

---
+++ tests/test_facecomp.py
@@ -0,0 +1,22 @@
+import src.cw_nnrec.facecomp as facecomp
+import os
+
+class TestFacecomp():
+    def test_compare_equal(self):
+        face1 = "tests/images/dicaprio1.jpg"
+        face2 = "tests/images/dicaprio2.jpg"
+
+        fc = facecomp.Facecomp()
+
+        assert fc.add_faces(face1, face2) == True
+        assert fc.compare() == True
+
+    def test_compare_not_equal(self):
+        face1 = "tests/images/dicaprio1.jpg"
+        face2 = "tests/images/depp.jpg"
+
+        fc = facecomp.Facecomp()
+
+        assert fc.add_faces(face1, face2) == True
+        assert fc.compare() == False
+
